d375dafecc2fffb31ee554e970894494b7092def
BadWolf <sunejax@gmail.com>
2018-03-21 03:01:57 +0530

    some changes to reg

--- login/register.php
+++ login/register.php
@@ -37,11 +37,11 @@
             <form class="login100-form validate-form" method="post" action="../login/register.php">
                 <?php include('errors.php'); ?>
                 <div class="wrap-input100 validate-input">
-                    <input class="input100" type="text" name="name">
+                    <input class="input100" type="text" name="username">
                     <span class="focus-input100" data-placeholder="Name"></span>
                 </div>
                 <div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
-                    <input class="input100" type="text" name="username">
+                    <input class="input100" type="text" name="email">
                     <span class="focus-input100" data-placeholder="Email"></span>
                 </div>
 
@@ -49,8 +49,15 @@
 						<span class="btn-show-pass">
 							<i class="zmdi zmdi-eye"></i>
 						</span>
-                    <input class="input100" type="password" name="password">
+                    <input class="input100" type="password" name="password_1">
                     <span class="focus-input100" data-placeholder="Password"></span>
+                </div>
+                <div class="wrap-input100 validate-input" data-validate="Enter password">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+                    <input class="input100" type="password" name="password_2">
+                    <span class="focus-input100" data-placeholder="Confirm Password"></span>
                 </div>
 
                 <div class="container-login100-form-btn">
