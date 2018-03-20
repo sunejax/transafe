78c0f75e07f295f4ef0e17b93ec1959bda2efa75
BadWolf <sunejax@gmail.com>
2018-03-20 19:37:57 +0530

    this

--- login/register.php
+++ login/register.php
@@ -1,16 +1,18 @@
 <?php include('server.php');
 require('../vendor/autoload.php');
 use Aws\S3\S3Client;
-// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
-$s3 = new S3Client([
-    'version'     => 'latest',
-    'region'      => 'us-west-2',
+
+/*$s3 = new S3Client([
     'credentials' => [
         'key'    => 'AKIAJFPPYA63IIRKDU2Q',
         'secret' => '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',
     ],
 
-]);
+]);*/
+$s3 = S3Client::factory(array(
+    'key'    => 'AKIAJFPPYA63IIRKDU2Q',
+    'secret' => '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'
+));
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 ?>
 <!DOCTYPE html>
