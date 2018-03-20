876df17f9ed829cc8a1e1c3bb4d951feae8f2d2d
BadWolf <sunejax@gmail.com>
2018-03-20 16:46:39 +0530

    upload test

--- login/register.php
+++ login/register.php
@@ -1,4 +1,9 @@
-<?php include('server.php') ?>
+<?php include('server.php');
+require('../vendor/autoload.php');
+// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+$s3 = Aws\S3\S3Client::factory();
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,5 +42,22 @@
 			Already a member? <a href="login.php">Sign in</a>
 		</p>
 	</form>
+
+
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    // FIXME: add more validation, e.g. using ext/fileinfo
+    try {
+        // FIXME: do not use 'name' for upload (that's the original filename from the user's computer)
+        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+        ?>
+        <p>Upload <a href="<?=htmlspecialchars($upload->get('ObjectURL'))?>">successful</a> :)</p>
+    <?php } catch(Exception $e) { ?>
+        <p>Upload error :(</p>
+    <?php } } ?>
+<h2>Upload a file</h2>
+<form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+    <input name="userfile" type="file"><input type="submit" value="Upload">
+</form>
 </body>
 </html>
