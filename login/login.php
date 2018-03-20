68ec2040377a1eb3ea0dbaa5bc1866c3709d6720
BadWolf <sunejax@gmail.com>
2018-03-20 20:56:28 +0530

    asd

--- login/login.php
+++ login/login.php
@@ -43,7 +43,7 @@
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
-						<input class="input100" type="text" name="email">
+						<input class="input100" type="text" name="username">
 						<span class="focus-input100" data-placeholder="Email"></span>
 					</div>
 
@@ -51,7 +51,7 @@
 						<span class="btn-show-pass">
 							<i class="zmdi zmdi-eye"></i>
 						</span>
-						<input class="input100" type="password" name="pass">
+						<input class="input100" type="password" name="password">
 						<span class="focus-input100" data-placeholder="Password"></span>
 					</div>
 
--- login/server.php
+++ login/server.php
@@ -57,13 +57,13 @@
 
 		if (count($errors) == 0) {
 			$password = md5($password);
-			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+			$query = "SELECT * FROM user WHERE email='$username' AND password='$password'";
 			$results = mysqli_query($db, $query);
 
 			if (mysqli_num_rows($results) == 1) {
 				$_SESSION['username'] = $username;
 				$_SESSION['success'] = "You are now logged in";
-				header('location: login/index.php');
+				header('location: index.php');
 			}else {
 				array_push($errors, "Wrong username/password combination");
 			}
