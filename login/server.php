8067cd59e942a36b4c16b6b950bccbceb6fdfe52
BadWolf <sunejax@gmail.com>
2018-03-20 20:45:23 +0530

    asd

--- login/server.php
+++ login/server.php
@@ -30,11 +30,11 @@
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
 			$password = md5($password_1);//encrypt the password before saving in the database
-			$query = "INSERT INTO user (name, email, password,em_no) 
+			$query = "INSERT INTO user (name, email, password) 
 					  VALUES('$name', '$email', '$password')";
 			mysqli_query($db, $query);
 
-			$_SESSION['username'] = $username;
+			$_SESSION['username'] = $name;
 			$_SESSION['success'] = "You are now logged in";
 			header('location: index.php');
 		}
