0396d0909c6bed9d3e6ca35a0899c662820ad07c
BadWolf <sunejax@gmail.com>
2018-03-20 16:40:13 +0530

    something

--- login/server.php
+++ login/server.php
@@ -2,7 +2,7 @@
 	session_start();
 
 	// variable declaration
-	$username = "";
+	$name = "";
 	$email    = "";
 	$errors = array(); 
 	$_SESSION['success'] = "";
@@ -13,13 +13,13 @@
 	// REGISTER USER
 	if (isset($_POST['reg_user'])) {
 		// receive all input values from the form
-		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$name = mysqli_real_escape_string($db, $_POST['username']);
 		$email = mysqli_real_escape_string($db, $_POST['email']);
 		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
 		// form validation: ensure that the form is correctly filled
-		if (empty($username)) { array_push($errors, "Username is required"); }
+		if (empty($name)) { array_push($errors, "Name is required"); }
 		if (empty($email)) { array_push($errors, "Email is required"); }
 		if (empty($password_1)) { array_push($errors, "Password is required"); }
 
@@ -30,8 +30,8 @@
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
 			$password = md5($password_1);//encrypt the password before saving in the database
-			$query = "INSERT INTO users (username, email, password) 
-					  VALUES('$username', '$email', '$password')";
+			$query = "INSERT INTO user (name, email, password,em_no) 
+					  VALUES('$name', '$email', '$password')";
 			mysqli_query($db, $query);
 
 			$_SESSION['username'] = $username;
