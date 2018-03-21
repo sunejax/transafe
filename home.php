e985e0e10b01b359ce3a7e12b847305bb42e81fc
BadWolf <sunejax@gmail.com>
2018-03-21 12:58:41 +0530

    homepage bug fix

--- home.php
+++ home.php
@@ -65,26 +65,6 @@ if (isset($_GET['logout'])) {
     </style>
 </head>
 <body>
-<!--<div class="content">-->
-<!---->
-<!--    <!-- notification message -->-->
-<!--    --><?php //if (isset($_SESSION['success'])) : ?>
-<!--        <div class="error success" >-->
-<!--            <h3>-->
-<!--                --><?php
-//                echo $_SESSION['success'];
-//                unset($_SESSION['success']);
-//                ?>
-<!--            </h3>-->
-<!--        </div>-->
-<!--    --><?php //endif ?>
-<!---->
-<!--    <!-- logged in user information -->-->
-<!--    --><?php // if (isset($_SESSION['username'])) : ?>
-<!--        <p>Welcome <strong>--><?php //echo $_SESSION['username']; ?><!--</strong></p>-->
-<!--        <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>-->
-
-
 <!-- banner -->
 <div class="banner">
     <!--header-->
