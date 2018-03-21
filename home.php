929006664923d36fce58ffdd699de9f8c32df098
BadWolf <sunejax@gmail.com>
2018-03-21 12:54:58 +0530

    homepage

--- home.php
+++ home.php
@@ -81,13 +81,12 @@ if (isset($_GET['logout'])) {
 <!--            </h3>-->
 <!--        </div>-->
 <!--    --><?php //endif ?>
+<!---->
+<!--    <!-- logged in user information -->-->
+<!--    --><?php // if (isset($_SESSION['username'])) : ?>
+<!--        <p>Welcome <strong>--><?php //echo $_SESSION['username']; ?><!--</strong></p>-->
+<!--        <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>-->
 
-    <!-- logged in user information -->
-    <?php  if (isset($_SESSION['username'])) : ?>
-        <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
-        <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-    <?php endif ?>
-</div>
 
 <!-- banner -->
 <div class="banner">
