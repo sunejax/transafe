053e4f988d44a4740a7ace033cb36ec367b5bef3
BadWolf <sunejax@gmail.com>
2018-03-21 12:24:31 +0530

    registration unique

--- login/server.php
+++ login/server.php
@@ -25,9 +25,11 @@
 		if (empty($email)) { array_push($errors, "Email is required"); }
 		if (empty($password_1)) { array_push($errors, "Password is required"); }
         if (empty($em_no)) { array_push($errors, "Emergency Contact is required"); }
-		if ($password_1 != $password_2) {
-			array_push($errors, "The two passwords do not match");
-		}
+		if ($password_1 != $password_2) {array_push($errors, "The two passwords do not match");}
+
+		$q="SELECT * FROM user WHERE email='$email'";
+		$r=mysqli_query($db,$q);
+        if (mysqli_num_rows($r)){array_push($errors, "Email already registered, contact admin for if you have forgotten the password");}
 
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
@@ -35,6 +37,7 @@
 			$query = "INSERT INTO user (name, email, password,em_no) 
 					  VALUES('$name', '$email', '$password','$em_no')";
 			mysqli_query($db, $query);
+
 
 			$_SESSION['username'] = $name;
 			$_SESSION['success'] = "You are now logged in";
