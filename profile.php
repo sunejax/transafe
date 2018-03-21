90844c44f88c787e2fe387fe67606677ca4cd9ef
BadWolf <sunejax@gmail.com>
2018-03-21 14:10:25 +0530

    this

--- profile.php
+++ profile.php
@@ -1,12 +1,12 @@
 <?php
 include('login/server.php');
 include('phpqrcode/qrlib.php');
-/*if (!isset($_SESSION['username'])) {
+if (!isset($_SESSION['username'])) {
     $_SESSION['msg'] = "You must log in first";
     header('location: login/login.php');
 
 }
-*/
+
 if (isset($_GET['logout'])) {
     session_destroy();
     unset($_SESSION['username']);
