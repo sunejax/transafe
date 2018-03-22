219288ec6d4fb7bc045c1f860c9b17b2204ddb18
BadWolf <sunejax@gmail.com>
2018-03-22 10:27:36 +0530

    something

--- profile.php
+++ profile.php
@@ -116,9 +116,6 @@ if(isset($_POST['uploadFile'])) {
                         <li><a href="profile.php" class="active"><?php echo $_SESSION['username']; ?></a></li>
                         <li><a href="profile.php?logout='1'">Logout</a></li>
                         <li><a href="http://driving-tests.in/learners-licence-practice-test-1/" >Take a Test</a></li>
-                        <li><a href="#team" class="scroll">Team</a></li>
-                        <li><a href="#news" class="scroll">News</a></li>
-                        <li><a href="#contact" class="scroll">Contact Us</a></li>
                     </ul>
                     <div class="clearfix"> </div>
                 </div>
