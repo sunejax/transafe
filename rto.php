4653580a3819cb61ca24d28eddec12afa1719834
BadWolf <sunejax@gmail.com>
2018-03-23 08:07:32 +0530

    upload query change

--- rto.php
+++ rto.php
@@ -5,7 +5,7 @@ $q="Select (name,email,em_no,doc_rc,doc_li,doc_aa) from user";
 $results=mysqli_query($db,$q);
 echo "<table>";
 
-while ($row_users = mysql_fetch_array($results)) {
+while ($row_users = $results->fetch_array(MYSQLI_ASSOC)) {
     //output a row here
     echo "<tr><td>".($row_users['email'])."</td></tr>";
 }
