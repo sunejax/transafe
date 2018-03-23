64101c290d0e22c32e0960464432cb17c24a4748
BadWolf <sunejax@gmail.com>
2018-03-23 08:12:30 +0530

    upload query change

--- rto.php
+++ rto.php
@@ -1,6 +1,13 @@
 <?php
 include('login/server.php');
 
+
+if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login/login.php');
+
+}
+
 $q="Select name,email,em_no,doc_rc,doc_li,doc_aa from user";
 $results=mysqli_query($db,$q);
 echo "<table>";
