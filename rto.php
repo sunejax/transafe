04e1c8aa6c1ce97d19dbb34dac3980d90f51d6af
BadWolf <sunejax@gmail.com>
2018-03-23 09:27:31 +0530

    about page changed

--- rto.php
+++ rto.php
@@ -8,6 +8,12 @@ if (!isset($_SESSION['username'])) {
 
 }
 
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login/login.php");
+}
 $q="Select name,email,em_no,doc_rc,doc_li,doc_aa from user WHERE ad_rights is NULL ";
 $results=mysqli_query($db,$q);
 ?>
@@ -36,8 +42,98 @@ $results=mysqli_query($db,$q);
             box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
         }
     </style>
+    <title>Transafe - <?php echo $_SESSION['r']['name']?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- bootstrap-css -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <!--// bootstrap-css -->
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+    <!--// css -->
+    <!-- font-awesome icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome icons -->
+    <!-- font -->
+    <link href='//fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    <!-- //font -->
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- light-box -->
+    <link rel="stylesheet" href="css/lightbox.css">
+    <!-- //light-box -->
+    <script src="js/SmoothScroll.min.js"></script>
+    <!--search jQuery-->
+    <script src="js/main.js"></script>
+    <!--//search jQuery-->
+    <script type="text/javascript">
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event){
+                event.preventDefault();
+                $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+            });
+        });
+    </script>
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+         * element that contains the map. */
+        #map {
+            height: 100%;
+        }
+        /* Optional: Makes the sample page fill the window. */
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+    <style>
+        img {
+            border: 1px solid #ddd;
+            border-radius: 4px;
+            padding: 5px;
+            width: 150px;
+        }
+
+        img:hover {
+            box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+        }
+    </style>
 </head>
 <body>
+<div class="banner">
+    <!--header-->
+    <div class="header">
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <h1><a  href="home.php">Transafe</a></h1>
+                </div>
+                <!--navbar-header-->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="home.php" >Home</a></li>
+                        <li><a href="profile.php" class="active"><?php echo $_SESSION['username']; ?></a></li>
+                        <li><a href="profile.php?logout='1'">Logout</a></li>
+                        <li><a href="http://driving-tests.in/learners-licence-practice-test-1/" >Take a Test</a></li>
+                    </ul>
+                    <div class="clearfix"> </div>
+                </div>
+            </nav>
+        </div>
+
+    </div>
+    <!--//header-->
+
+</div>
 <table>
    <? while ($row_users = $results->fetch_array(MYSQLI_ASSOC)) {
     //output a row here
