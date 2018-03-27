cd9a373d15e2f93af083b24fd5a2e46cd927818a
BadWolf <sunejax@gmail.com>
2018-03-27 18:15:02 +0530

    quiz login

--- quiz.php
+++ quiz.php
@@ -40,17 +40,7 @@ if (isset($_GET['logout'])) {
 <div class="scp-quizzes-main">
 <div class="scp-quizzes-data">
   <h3>1. What is the output of the below c code?</h3>
-    <pre>#include&lt;stdio.h>	
-main() 
-{
-   int x = 5;
-   
-   if(x=5)
-   {	
-      if(x=5) printf("Fast");
-   } 
-   printf("learning");
-}</pre>
+
 <br/>
     <input type="radio" id="Fastlearning" name="question1">
        <label for="Fastlearning">1. Fastlearning</label><br/>
@@ -63,17 +53,7 @@ main()
  </div>
  <div class="scp-quizzes-data">
   <h3>2. How would you insert pre-written code into a current program ?</h3>
-    <pre>#include&lt;stdio.h>	
-main() 
-{
-   int x = 5;
-   
-   if(x=5)
-   {	
-      if(x=5) printf("Fast");
-   } 
-   printf("learning");
-}</pre>
+
 <br/>
     <input type="radio" name="question2">
        <label>1. #read&lt;file></label><br/>
