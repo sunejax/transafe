cb8af48c249cdb4760a7a40d112b25f008b24c47
BadWolf <sunejax@gmail.com>
2018-03-27 18:12:50 +0530

    quiz login

--- quiz.php
+++ quiz.php
@@ -1,3 +1,20 @@
+<?php
+include('login/server.php');
+
+
+if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login/login.php');
+
+}
+
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login/login.php");
+}
+?>
 <!DOCTYPE html>
 <html>
    <head>
