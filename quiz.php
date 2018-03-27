cae7e430b1069fc1e3ddf175b4f710a97dac5ddc
BadWolf <sunejax@gmail.com>
2018-03-27 18:22:17 +0530

    quiz login

--- quiz.php
+++ quiz.php
@@ -39,30 +39,25 @@ if (isset($_GET['logout'])) {
    <body >
 <div class="scp-quizzes-main">
 <div class="scp-quizzes-data">
-  <h3>1. What is the output of the below c code?</h3>
-
+  <h3>1. You are approaching a narrow bridge, another vehicle is about to enter the bridge from opposite side, you</h3>
 <br/>
     <input type="radio" id="Fastlearning" name="question1">
-       <label for="Fastlearning">1. Fastlearning</label><br/>
+       <label for="Fastlearning">1. Increase the speed and try to cross the bridge as fast as possible </label><br/>
     <input type="radio"  name="question1">
-       <label>2. learning</label><br/>
+       <label>2. Put on the headlight and pass the bridge </label><br/>
     <input type="radio" name="question1">
-       <label>3. learningFast</label> <br/>
-    <input type="radio" name="question1">
-     <label>4. Fast</label> 
+       <label>3. Wait till the other vehicle crosses the bridge and then proceed</label> <br/>
  </div>
  <div class="scp-quizzes-data">
-  <h3>2. How would you insert pre-written code into a current program ?</h3>
+  <h3>2. Near a pedestrian crossing, when the pedestrians are waiting to cross the road, you should</h3>
 
 <br/>
     <input type="radio" name="question2">
-       <label>1. #read&lt;file></label><br/>
+       <label>1. Sound horn and proceed.</label><br/>
     <input type="radio"  name="question2">
-       <label>2. #get &lt;file></label><br/>
+       <label>2. Slow down, sound horn and pass.</label><br/>
     <input type="radio" name="question2" id="inculdefile">
-       <label for="inculdefile">3. #include &lt;file></label> <br/>
-    <input type="radio" name="question2">
-     <label>4. #pre &lt;file></label> 
+       <label for="inculdefile">3. Stop the vehicle and wait till the pedestrians cross the road and then proceed. </label> <br/>
  </div>
 </div>
 
