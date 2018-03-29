dd3cabdf20c81cdc54bfef26765af23b32479272
BadWolf <sunejax@gmail.com>
2018-03-30 04:17:08 +0530

    db links finished

--- rto.php
+++ rto.php
@@ -91,6 +91,54 @@ if (isset($_POST['accept'])) {
             });
         });
     </script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Transafe - <? echo $_SESSION['username'] ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Transafe - Your Personal Travel Safety Manager" />
+
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+    <script src="js/jquery.min.js"></script>
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Simple Line Icons -->
+    <link rel="stylesheet" href="css/simple-line-icons.css">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Styleswitcher ( This style is for demo purposes only, you may delete this anytime. ) -->
+    <link rel="stylesheet" id="theme-switch" href="css/style.css">
+    <!-- End demo purposes only -->
+
+
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
     <style>
         /* Always set the map height explicitly to define the size of the div
          * element that contains the map. */
@@ -122,36 +170,27 @@ if (isset($_POST['accept'])) {
     <script type='text/javascript' src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap2-toggle.min.js"></script>
 </head>
 <body>
-<div class="banner">
-    <!--header-->
-    <div class="header">
-        <div class="container">
-            <nav class="navbar navbar-default">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <h1><a  href="rto.php">Transafe</a></h1>
-                </div>
-                <!--navbar-header-->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="rto.php" class="active"><?php echo $_SESSION['username']; ?></a></li>
-                        <li><a href="history.php">History</a></li>
-                        <li><a href="rto.php?logout='1'">Logout</a></li>
-                    </ul>
-                    <div class="clearfix"> </div>
-                </div>
-            </nav>
-        </div>
-
+<header role="banner" id="fh5co-header">
+    <div class="container">
+        <!-- <div class="row"> -->
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <!-- Mobile Toggle Menu Button -->
+                <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
+                <a class="navbar-brand" href="index.html">Transafe</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="home.php"><span>Home</span></a></li>
+                    <li><a href="quiz.php"><span>Practice</span></a></li>
+                    <li class="active" data-nav-section="home"><a href="profile.php"><?php echo $_SESSION['username']; ?></a></li>
+                    <li><a href="profile.php?logout='1'">Sign out</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- </div> -->
     </div>
-    <!--//header-->
-
-</div>
+</header>
 <table>
     <? while ($row_users = $results->fetch_array(MYSQLI_ASSOC)) {
         //output a row here
@@ -207,6 +246,36 @@ if (isset($_POST['accept'])) {
         });
     });
 </script>
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<!-- Owl Carousel -->
+<script src="js/owl.carousel.min.js"></script>
+
+<!-- For demo purposes only styleswitcher ( You may delete this anytime ) -->
+<script src="js/jquery.style.switcher.js"></script>
+<script>
+    $(function(){
+        $('#colour-variations ul').styleSwitcher({
+            defaultThemeId: 'theme-switch',
+            hasPreview: false,
+            cookie: {
+                expires: 30,
+                isManagingLoad: true
+            }
+        });
+        $('.option-toggle').click(function() {
+            $('#colour-variations').toggleClass('sleep');
+        });
+    });
+</script>
+<!-- End demo purposes only -->
+
+<!-- Main JS (Do not remove) -->
+<script src="js/main.js"></script>
 </body>
 
 </html>
