2d5443f160e280c1a29a7e67831e1562c653427c
BadWolf <sunejax@gmail.com>
2018-03-30 00:04:27 +0530

    db links finished

--- profile.php
+++ profile.php
@@ -231,34 +231,7 @@ if(isset($_POST['uploadFile_aa'])) {
                     <p><?echo$_SESSION['r']['em_msg']?></p>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
-                <div class="fh5co-icon"><i class="icon-speedometer"></i></div>
-                <div class="fh5co-desc">
-                    <h3>Lightweight</h3>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-                </div>
-            </div>
             <div class="clearfix visible-sm-block visible-xs-block"></div>
-            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
-                <div class="fh5co-icon"><i class="icon-heart"></i></div>
-                <div class="fh5co-desc">
-                    <h3>Made with Love</h3>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
-                <div class="fh5co-icon"><i class="icon-umbrella"></i></div>
-                <div class="fh5co-desc">
-                    <h3>Eco Friendly</h3>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
-                </div>
-            </div>
-            <div class="clearfix visible-sm-block visible-xs-block"></div>
-        </div>
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4 single-animate animate-features-3">
-                <a href="#" class="btn btn-primary btn-block">Learn More</a>
-            </div>
         </div>
     </div>
 </div>
