9ff5811944b1da9199890ff39d6e0be00ca50fe0
BadWolf <sunejax@gmail.com>
2018-03-29 23:54:40 +0530

    db links finished

--- profile.php
+++ profile.php
@@ -179,7 +179,6 @@ if(isset($_POST['uploadFile_aa'])) {
     </div>
 </header>
 <div id="slider" data-section="home">
-
     <!-- You may change the background color here. -->
     <div class="item" style="background: #352f44;">
         <div class="container" style="position: relative;">
@@ -198,10 +197,73 @@ if(isset($_POST['uploadFile_aa'])) {
             </div>
         </div>
     </div>
-
-
 </div>
-
+<div id="fh5co-features" data-section="features">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 section-heading text-center">
+                <h2 class="single-animate animate-features-1">Features</h2>
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 single-animate animate-features-2">
+                        <h3><b>Name</b><?echo$_SESSION['r']['name']?></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-bottom-padded-sm">
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
+                <div class="fh5co-icon"><i class="icon-present"></i></div>
+                <div class="fh5co-desc">
+                    <h3>100% Free</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
+                <div class="fh5co-icon"><i class="icon-eye"></i></div>
+                <div class="fh5co-desc">
+                    <h3>Retina Ready</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                </div>
+            </div>
+            <div class="clearfix visible-sm-block visible-xs-block"></div>
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
+                <div class="fh5co-icon"><i class="icon-crop"></i></div>
+                <div class="fh5co-desc">
+                    <h3>Fully Responsive</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
+                <div class="fh5co-icon"><i class="icon-speedometer"></i></div>
+                <div class="fh5co-desc">
+                    <h3>Lightweight</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                </div>
+            </div>
+            <div class="clearfix visible-sm-block visible-xs-block"></div>
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
+                <div class="fh5co-icon"><i class="icon-heart"></i></div>
+                <div class="fh5co-desc">
+                    <h3>Made with Love</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 col-xxs-12 fh5co-service to-animate">
+                <div class="fh5co-icon"><i class="icon-umbrella"></i></div>
+                <div class="fh5co-desc">
+                    <h3>Eco Friendly</h3>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                </div>
+            </div>
+            <div class="clearfix visible-sm-block visible-xs-block"></div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4 single-animate animate-features-3">
+                <a href="#" class="btn btn-primary btn-block">Learn More</a>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="row">
     <div class="col-sm-12">
         <ul class="list-group" style="color: #000000">
