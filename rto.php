31f71e711c817bda1a0a23963c2ca763ebc04aed
BadWolf <sunejax@gmail.com>
2018-03-30 04:51:13 +0530

    db links finished

--- rto.php
+++ rto.php
@@ -224,9 +224,9 @@ if (isset($_POST['accept'])) {
         if($li_s=='2')$str_li='checked';
         if($aa_s=='2')$str_aa='checked';
         echo "<tr><td>".($row_users['name'])."</td><td>".($row_users['email'])."</td><td>".($row_users['em_no'])."</td>
-<td style='width: 200px'><a target='_blank' href=$rc><img src =$rc></a><input name='doc_rc_s' class='switcher' id=$uid data-toggle='toggle' $str_rc data-on='Accept' data-width=100 data-height=34 data-off='Decline' data-onstyle='success' data-offstyle='danger' type='checkbox'></td>
-<td style='width: 200px'><a target='_blank' href=$li><img src =$li></a><input name='doc_li_s' class='switcher' id=$uid data-toggle='toggle' $str_li data-on='Accept' data-width=100 data-height=34 data-off='Decline' data-onstyle='success' data-offstyle='danger' type='checkbox'></td>
-<td style='width: 200px'><a target='_blank' href=$aa><img src =$aa></a><input name='doc_aa_s' class='switcher' id=$uid data-toggle='toggle' $str_aa data-on='Accept' data-width=100 data-height=34 data-off='Decline' data-onstyle='success' data-offstyle='danger' type='checkbox'></td>
+<td style='width: 200px'><a target='_blank' href=$rc><img src =$rc></a><input name='doc_rc_s' class='switcher' id=$uid data-toggle='toggle' $str_rc data-on='Accept' data-width=100 data-height=34 data-off='Decline' data-onstyle='success' data-offstyle='danger' type='checkbox' style='padding: 8px'></td>
+<td style='width: 200px'><a target='_blank' href=$li><img src =$li></a><input name='doc_li_s' class='switcher' id=$uid data-toggle='toggle' $str_li data-on='Accept' data-width=100 data-height=34 data-off='Decline' data-onstyle='success' data-offstyle='danger' type='checkbox' style='padding: 8px'></td>
+<td style='width: 200px'><a target='_blank' href=$aa><img src =$aa></a><input name='doc_aa_s' class='switcher' id=$uid data-toggle='toggle' $str_aa data-on='Accept' data-width=100 data-height=34 data-off='Decline' data-onstyle='success' data-offstyle='danger' type='checkbox' style='padding: 8px'></td>
 </tr>";
         ;}
     ?>
