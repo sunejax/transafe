9ec3b3ec592f710db2e3ca4d3f6adba709a48d14
BadWolf <sunejax@gmail.com>
2018-04-01 17:22:23 +0530

    rto page changes

--- quiz.php
+++ quiz.php
@@ -133,3 +133,69 @@
         </div>
     </div>
 </div>
+<footer id="footer" role="contentinfo">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <ul class="social social-circle">
+                    <li><a href="#"><i class="icon-twitter"></i></a></li>
+                    <li><a href="#"><i class="icon-facebook"></i></a></li>
+                    <li><a href="#"><i class="icon-youtube"></i></a></li>
+                    <li><a href="#"><i class="icon-pinterest"></i></a></li>
+                    <li><a href="#"><i class="icon-linkedin"></i></a></li>
+                    <li><a href="#"><i class="icon-instagram"></i></a></li>
+                    <li><a href="#"><i class="icon-dribbble"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- //footer -->
+<script src="js/jarallax.js"></script>
+<script type="text/javascript">
+    /* init Jarallax */
+    $('.jarallax').jarallax({
+        speed: 0.5,
+        imgWidth: 1366,
+        imgHeight: 768
+    })
+</script>
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<!-- here stars scrolling icon -->
+<!-- //here ends scrolling icon -->
+
+<!-- jQuery -->
+
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<!-- Owl Carousel -->
+<script src="js/owl.carousel.min.js"></script>
+
+<!-- For demo purposes only styleswitcher ( You may delete this anytime ) -->
+<script src="js/jquery.style.switcher.js"></script>
+<script>
+    $(function(){
+        $('#colour-variations ul').styleSwitcher({
+            defaultThemeId: 'theme-switch',
+            hasPreview: false,
+            cookie: {
+                expires: 30,
+                isManagingLoad: true
+            }
+        });
+        $('.option-toggle').click(function() {
+            $('#colour-variations').toggleClass('sleep');
+        });
+    });
+</script>
+<!-- End demo purposes only -->
+
+<!-- Main JS (Do not remove) -->
+<script src="js/main.js"></script>
+</body>
+</html>
