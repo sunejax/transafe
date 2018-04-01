a6b389222304991bd8bfdee9fa9122df27555a48
BadWolf <sunejax@gmail.com>
2018-04-01 23:29:35 +0530

    changed a litty bitty

--- history.php
+++ history.php
@@ -209,7 +209,7 @@ if (isset($_POST['accept'])) {
 </div>
 <br>
 <table>
-    <tr><td><b>Name</b></td><td><b>Email</b></td><td><b>Emergency Contact</b></td><td><b> Score</b></td>
+    <tr><td><b>Name</b></td><td><b>Email</b></td><td><b>Emergency Contact</b></td><td><b> Score(5)</b></td>
         <td style='width: 200px;'><b>Registration Certificate</b></td>
         <td style='width: 200px;'><b>License</b></td>
         <td style='width: 200px;'><b>AADHAR</b></td>
