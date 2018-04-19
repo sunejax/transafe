ab18b54cff6ade7b77256561bb1a3ff443d7e499
BadWolf <sunejax@gmail.com>
2018-04-20 01:08:33 +0530

    registration page changes

--- login/server.php
+++ login/server.php
@@ -34,9 +34,10 @@ if (isset($_POST['reg_user'])) {
 
     // register user if there are no errors in the form
     if (count($errors) == 0) {
+        $activation = md5(uniqid(rand(), true));
         $password = md5($password_1);//encrypt the password before saving in the database
-        $query = "INSERT INTO user (name, email, password,em_no,li_pl) 
-					  VALUES('$name', '$email', '$password','$em_no','$li_pl')";
+        $query = "INSERT INTO user (name, email, password,em_no,li_pl,pl_key) 
+					  VALUES('$name', '$email', '$password','$em_no','$li_pl','$activation')";
         mysqli_query($db, $query);
 
 
