0dc4254a333e6802c7206915131cc737a011944c
BadWolf <sunejax@gmail.com>
2018-04-23 05:14:27 +0530

    footer removed

--- quiz.php
+++ quiz.php
@@ -185,21 +185,7 @@ if(isset($_POST['score'])) {
 </div>
 
 <footer id="footer" role="contentinfo">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <ul class="social social-circle">
-                    <li><a href="#"><i class="icon-twitter"></i></a></li>
-                    <li><a href="#"><i class="icon-facebook"></i></a></li>
-                    <li><a href="#"><i class="icon-youtube"></i></a></li>
-                    <li><a href="#"><i class="icon-pinterest"></i></a></li>
-                    <li><a href="#"><i class="icon-linkedin"></i></a></li>
-                    <li><a href="#"><i class="icon-instagram"></i></a></li>
-                    <li><a href="#"><i class="icon-dribbble"></i></a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+
 </footer>
 <!-- //footer -->
 <script src="js/jarallax.js"></script>
