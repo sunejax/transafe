cc0105e9f20c5366b579cb4b2d173cc1ab5f1053
BadWolf <sunejax@gmail.com>
2018-04-23 01:54:07 +0530

    police page transafe fixed

--- police.php
+++ police.php
@@ -194,7 +194,7 @@ if (isset($_POST['poli_sub'])) {
             <div class="navbar-header">
                 <!-- Mobile Toggle Menu Button -->
                 <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i></i></a>
-                <a class="navbar-brand" href="rto.php">Transafe</a>
+                <a class="navbar-brand" href="police.php">Transafe</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
